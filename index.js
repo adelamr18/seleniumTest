9683ff38605ffbf04ddb7668ab5fb4e332d3561b
a.gebril <a.gebril@metro-markets.de>
2022-01-18 18:06:40 +0100

    Added error handling

--- index.js
+++ index.js
@@ -8,18 +8,40 @@ async function checkIfTitleExistsAndNavigate() {
     .setChromeOptions(new chrome.Options().addArguments("--headless"))
     .setChromeOptions(new chrome.Options().addArguments("--no-sandbox"))
     .build();
-  await driver.get("https://phptravels.com/demo");
-  const headerText = await (
+  const getMainRoute = await driver.get("https://phptravels.com/demo");
+  const [mainRoute, mainRouteError] = await promiseResolver(getMainRoute);
+  const getHeaderTitle = await (
     await driver.findElement(By.id("header-title"))
   ).getAttribute("innerHTML");
-  assert.equal("Application Test Drive", headerText);
+  const [headerTitle, headerTitleError] = await promiseResolver(getHeaderTitle);
+
+  if (mainRouteError || headerTitleError) {
+    console.log("An error occured while executing this script: " + mainRouteError +
+    headerTitleError );
+  }
+
+  assert.equal("Application Test Drive", getHeaderTitle);
   pressOnIntegrationsTab(driver);
 }
 
 async function pressOnIntegrationsTab(driver) {
-  await (await driver.findElement(By.className("clearfix"))).click();
+  const clickOnIntegrationsTab = await (await driver.findElement(By.className("clearfix"))).click();
+  const [integrationsTabClick, integrationsTabClickError] = await promiseResolver(clickOnIntegrationsTab);
+
+  if (integrationsTabClickError) {
+  console.log("An error occured while clicking on integrations tab: " + integrationsTabClickError);
+  }
 }
 
 (async function () {
   return await checkIfTitleExistsAndNavigate();
 })();
+
+async function promiseResolver(promise) {
+  try {
+    const data = await new Promise ((resolve, reject) => {resolve (1);});
+    return [data, null];
+  } catch (err) {
+    return [null, err];
+  }
+}
