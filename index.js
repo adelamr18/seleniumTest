5b4dc4d1a06eab317f27daeb0f706e72c603c986
a.gebril <a.gebril@metro-markets.de>
2022-01-18 13:52:32 +0100

    Add chrome options

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const { Builder, By } = require("selenium-webdriver");
-var assert = require('assert');
+var assert = require("assert");
+const chrome = require("selenium-webdriver/chrome");
 
 async function checkIfTitleExistsAndNavigate() {
-const driver = await new Builder().forBrowser("chrome").build();
-await driver.get('https://phptravels.com/demo');
-const headerText = await (await driver.findElement(By.id('header-title'))).getAttribute("innerHTML");
-assert.equal('Application Test Drive', headerText);
-pressOnIntegrationsTab(driver);
+  const driver = await new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(new chrome.Options().addArguments("--headless"))
+    .setChromeOptions(new chrome.Options().addArguments("--no-sandbox"))
+    .build();
+  await driver.get("https://phptravels.com/demo");
+  const headerText = await (
+    await driver.findElement(By.id("header-title"))
+  ).getAttribute("innerHTML");
+  assert.equal("Application Test Drive", headerText);
+  pressOnIntegrationsTab(driver);
 }
-async function pressOnIntegrationsTab(driver){
-await (await driver.findElement(By.className("clearfix"))).click();
+
+async function pressOnIntegrationsTab(driver) {
+  await (await driver.findElement(By.className("clearfix"))).click();
 }
 
 (async function () {
-    return await checkIfTitleExistsAndNavigate();
+  return await checkIfTitleExistsAndNavigate();
 })();
