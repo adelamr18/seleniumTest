754880f11c561ee8462bc964aca07901a6451156
a.gebril <a.gebril@metro-markets.de>
2022-01-27 11:39:25 +0100

    testing new config

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
-const { Builder, By } = require("selenium-webdriver");
+const { Builder, By, ChromeOptions } = require("selenium-webdriver");
 var assert = require("assert");
+// To prevenet the need to install chromedriver on the agent or doing any PATH handling
 const chrome = require("selenium-webdriver/chrome");
+const chromedriver = require("chromedriver");
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+
 
 async function checkIfTitleExistsAndNavigate() {
-  const driver = await new Builder()
-    .forBrowser("chrome")
-    .setChromeOptions(new chrome.Options().addArguments("--headless"))
-    .setChromeOptions(new chrome.Options().addArguments("--no-sandbox"))
-    .build();
-  const getMainRoute = await driver.get("https://phptravels.com/demo");
-  const [mainRoute, mainRouteError] = await promiseResolver(getMainRoute);
-  const getHeaderTitle = await (
-    await driver.findElement(By.id("header-title"))
-  ).getAttribute("innerHTML");
-  const [headerTitle, headerTitleError] = await promiseResolver(getHeaderTitle);
-
-  if (mainRouteError || headerTitleError) {
-    console.log("An error occured while executing this script: " + mainRouteError +
-    headerTitleError );
-  }
-
-  assert.equal("Application Test Drive", getHeaderTitle);
-  pressOnIntegrationsTab(driver);
+    // Use headless chrome for automations
+    const options = new chrome.Options();
+    options.addArguments("--headless");
+    options.addArguments("--no-sandbox");
+    
+    const driver = await new Builder().forBrowser("chrome").setChromeOptions(options).build();
+    await driver.get("https://phptravels.com/demo");
+    const headerText = await (await driver.findElement(By.id("header-title"))).getAttribute("innerHTML");
+    assert.equal("Application Test Drive", headerText);
+    await pressOnIntegrationsTab(driver);
+    driver.quit();
 }
-
 async function pressOnIntegrationsTab(driver) {
-  const clickOnIntegrationsTab = await (await driver.findElement(By.className("clearfix"))).click();
-  const [integrationsTabClick, integrationsTabClickError] = await promiseResolver(clickOnIntegrationsTab);
-
-  if (integrationsTabClickError) {
-  console.log("An error occured while clicking on integrations tab: " + integrationsTabClickError);
-  }
+    await (await driver.findElement(By.className("clearfix"))).click();
 }
 
-(async function () {
-  return await checkIfTitleExistsAndNavigate();
-})();
-
-async function promiseResolver(promise) {
-  try {
-    const data = await new Promise ((resolve, reject) => {resolve (1);});
-    return [data, null];
-  } catch (err) {
-    return [null, err];
-  }
-}
+checkIfTitleExistsAndNavigate().catch(err=>{
+    console.error(err);
+    process.exit(1);
+});
